db1067d349c77004179c066488aa594a32c4d7dc
Kalvin Zhao <kalvinzhao@kalvins-mbp.home>
2020-07-22 18:11:16 -0400

    added prototype compatability

--- index.js
+++ index.js
@@ -12,13 +12,12 @@ function Airplane(name) {
   this.name = name;
   this.isFlying = false;
 }
-Airplane.prototype.takeOff = function () {
+Airplane.prototype.takeOff = function() {
   this.isFlying = true;
 };
-Airplane.prototype.land = function () {
+Airplane.prototype.land = function() {
   this.isFlying = false;
 };
-
 
 /*
 // 👇 COMPLETE YOUR WORK BELOW 👇
@@ -38,10 +37,28 @@ Airplane.prototype.land = function () {
     - Give instances of Person a method `.toString()`:
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
-
-function Person() {
-
+Person.prototype.eat = function(edible) {
+  if (this.stomach.length < 10) {
+    this.stomach.push(edible);
+  }
+};
+Person.prototype.poop = function() {
+  this.stomach = [];
+};
+Person.prototype.toString = function() {
+  return `${this.name}, ${this.age}`;
+};
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
 }
+
+// const personOne = new Person("kalvin", 21);
+// console.log(personOne.toString());
+//creates a constructor function
+// it take 3 properties name, age, and an array
+// name and age need to be passed in as argument
 
 /*
   TASK 2
@@ -56,9 +73,24 @@ function Person() {
     - STRETCH: A car which runs out of `fuel` while driving can't drive any more distance:
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
-
-function Car() {
-
+Car.prototype.fill = function(gallons) {
+  this.tank += gallons;
+};
+Car.prototype.drive = function(distance) {
+  this.odometer += distance;
+  if (this.milesPerGallon >= this.odometer)
+  if (this.milesPerGallon === distance) {
+    this.tank -= this.milesPerGallon;
+  }
+  if (this.tank === 0) {
+    return `I ran out of fuel at ${this.odometer} miles!`;
+  }
+};
+function Car(model, milesPerGallon) {
+  this.tank = 0;
+  this.odometer = 0;
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
 }
 
 /*
@@ -68,28 +100,45 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
-
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.play = function() {
+  return `Playing with ${this.favoriteToy}`;
+};
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age);
+  this.person = {
+    name,
+    age
+  };
+  this.favoriteToy = favoriteToy;
 }
+console.log(Baby.prototype());
 
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
+  1. when it's outside of an funtion  or object, it will be the object of the window
+  2. the value preceding the "." will be what this refers to
   3. 
   4. 
 */
 
-
 ///////// END OF CHALLENGE /////////
 ///////// END OF CHALLENGE /////////
 ///////// END OF CHALLENGE /////////
-if (typeof exports !== 'undefined') {
-  module.exports = module.exports || {}
-  if (Airplane) { module.exports.Airplane = Airplane }
-  if (Person) { module.exports.Person = Person }
-  if (Car) { module.exports.Car = Car }
-  if (Baby) { module.exports.Baby = Baby }
+if (typeof exports !== "undefined") {
+  module.exports = module.exports || {};
+  if (Airplane) {
+    module.exports.Airplane = Airplane;
+  }
+  if (Person) {
+    module.exports.Person = Person;
+  }
+  if (Car) {
+    module.exports.Car = Car;
+  }
+  if (Baby) {
+    module.exports.Baby = Baby;
+  }
 }
